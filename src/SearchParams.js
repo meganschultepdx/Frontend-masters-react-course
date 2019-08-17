46a93e7d18c9f904e8705a798eedb79d025002dc
megan schulte <megan.schulte.pdx@gmail.com>
2019-08-17 16:37:01 -0700

    add animal search with the animal api array

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  {
+    /* line below is a hook, hooks begin with use... they never go inside of if statements or for loops */
+  }
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
 
   return (
     <div className="search-params">
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
@@ -14,7 +20,24 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
