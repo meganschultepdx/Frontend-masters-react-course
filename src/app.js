f41cc565ab9d47dcf7bc8fca6b6ff37c4588c998
megan schulte <megan.schulte.pdx@gmail.com>
2019-08-17 13:08:13 -0700

    add eslint

--- src/app.js
+++ src/app.js
@@ -1,34 +1,30 @@
 const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, name), 
+    React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed)
   ]);
-}
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { 
-        name: "Luna", 
-        animal: "dog", 
-        breed: "mix"
-      }),
-      React.createElement(Pet, { 
-        name: "Pepper", 
-        animal: "bird", 
-        breed: "mix"
-      }),
-      React.createElement(Pet, { 
-        name: "Fuzz", 
-        animal: "cat", 
-        breed: "mix"
-      })
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "dog",
+      breed: "mix"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "bird",
+      breed: "mix"
+    }),
+    React.createElement(Pet, {
+      name: "Fuzz",
+      animal: "cat",
+      breed: "mix"
+    })
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
